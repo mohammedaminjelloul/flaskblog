a9925741665a03dabe8992ef3b035cd74dc85cbc
mohammed-amin-jelloul <76473058+mohammed-amin-jelloul@users.noreply.github.com>
2021-01-04 18:04:26 +0100

    Add files via upload

---
+++ flask_blog/flaskblog/config.py
@@ -0,0 +1,10 @@
+import os
+class Config:
+	SECRET_KEY = 'a9a6e3f523d7bbe2423c7fdf4ebc6dfd'
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+	MAIL_SERVER = 'smtp.googlemail.com'
+	MAIL_PORT = 587
+	MAIL_USE_TLS = True
+	MAIL_USERNAME = 'lyceepilotedegafsa.blog@gmail.com'
+	MAIL_PASSWORD = 'lpgbyamin95139795'
+		
